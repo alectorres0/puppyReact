5da7021d21956c1468dd5c6c4d40be21b342b9f9
Alec <alectorres0@gmail.com>
2024-01-21 11:10:40 -0800

    featured

--- myFirstReactApp/src/App.jsx
+++ myFirstReactApp/src/App.jsx
@@ -2,10 +2,24 @@ import {puppyList} from "./data.js"
 import { useState } from 'react'
 const App = () =>{
   const [puppies, setPuppies] = useState(puppyList);
+  const [featPupId,setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=>pup.id === featPupId);
+  console.log(featuredPup);
   console.log(puppies);
 return <div>{puppies.map((puppy)=>{
-  return <p key ={puppy.id}>{puppy.name}</p>
-})}</div>
+  return <p onClick = {()=>{setFeatPupId(puppy.id)}} key ={puppy.id}>{puppy.name}</p>
+})
+  }
+  {featPupId && (
+  <div>
+    <h2>{featuredPup.name}</h2>
+    <ul>
+      <li>Age: {featuredPup.age}</li>
+      <li>Email: {featuredPup.email}</li>
+    </ul>
+  </div>)}
+  </div>
+
 }
 
 export default App;
